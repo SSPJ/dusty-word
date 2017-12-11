985e1322cbdb54712434514f388539bb7b18004e
Seamus <git@seamusjohnston.com>
2017-12-11 18:24:48 -0500

    :w

--- word_test.sh
+++ word_test.sh
@@ -5,3 +5,12 @@
   [ "$status" -eq 0 ]
   [ "${lines[0]}" = 'Usage: word [options] [primary modifiers] <word|phrase> \' ]
 }
+
+# At a high level, I want the app to perform these functions
+# A. Given a word
+# -- tell me what word I'm trying to spell
+# -- tell me what a word means
+# -- tell me how to say a word
+# B. Given a word or a phrase
+# -- tell me words that are like it
+
